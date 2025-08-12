3bbbfe43cace6d2bf5984ff5df88d46d9a759635
Camba <fercambafcm@gmail.com>
2025-08-11 22:26:48 -0600

    Fixes for vercel 4

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,11 @@
-process.loadEnvFile()
+// Solo carga .env en desarrollo local
+if (process.env.NODE_ENV !== "production") {
+    try {
+        await import("dotenv").then((dotenv) => dotenv.config())
+    } catch (err) {
+        console.warn("⚠️ dotenv no cargado:", err)
+    }
+}
 
-export const {MONGO_URL, APP_PORT, API_KEY} = process.env
+export const API_KEY = process.env.API_KEY
+export const MONGO_URL = process.env.MONGO_URL
